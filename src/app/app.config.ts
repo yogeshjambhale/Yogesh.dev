6eff067e5cd97a435b5827db140b223410398d1d
Yogesh Rajendra Jambhale <yogesh@Yogeshs-MacBook-Air.local>
2026-03-30 22:37:56 +0530

    Upgrade to @angular/ssr for World-Class SEO and performance

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideClientHydration(withEventReplay())
   ]
 };
